2a0b7b3a35c8173ecc30e6dac7bc0ee18d1f01bc
Jaume Giralt Barbé <jaume.giralt.2012@gmail.com>
2017-04-18 17:26:02 +0200

    Add urls

--- FilmRevolution/urls.py
+++ FilmRevolution/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^FilmRevolutionApp', include('FilmRevolutionApp.urls',
+                                       namespace='FilmRevolutionApp')),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
 ]
