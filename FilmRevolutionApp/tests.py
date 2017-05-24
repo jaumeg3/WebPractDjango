00ec74f6e5bc5d9b00efcfee05e0319c4bc51b89
WhitenAND07 <jordi14and@gmail.com>
2017-05-24 18:20:43 +0200

    Add files via upload

--- FilmRevolutionApp/tests.py
+++ FilmRevolutionApp/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from models import MovieReview, Movie
+
 
 # Create your tests here.
+class MovieReviewTestCase(TestCase):
+    def setUp(self):
+        trendy = Movie.objects.create(title="Trendy	Movie", popularity=50)
+        user1 = User.objects.create(user="user1")
+        user2 = User.objects.create(user="user2")
+        user3 = User.objects.create(user="user3")
+        MovieReview.objects.create(rating=3, comment="Average...",\
+                                   movie=trendy, user=user1)
+        MovieReview.objects.create(rating=5, comment="Excellent!",\
+                                   movie=trendy, user=user2)
+        MovieReview.objects.create(rating=1, comment="Really bad!",\
+                                   movie=trendy, user=user3)
+        Movie.objects.create(title="Unknown	Movie", popularity=41)
+
+    def test_average_3reviews(self):
+        """The	average	review	for	a	movie	with	3	reviews	is	properly	computed"""
+        movie = Movie.objects.get(title="Trendy Restaurant")
+        self.assertEqual(movie.averageRating(), 3)
+
+    def test_average_no_review(self):
+        """The average	review	for	a Movie	without	reviews	is	0"""
+        movie = Movie.objects.get(title="Unknown Restaurant")
+        self.assertEqual(movie.averageRating(), 0)
