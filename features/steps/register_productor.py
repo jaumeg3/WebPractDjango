78e48f50a68697dad175967ebc73155b8f21b34c
Jaume Giralt Barbé <jaume.giralt.2012@gmail.com>
2017-05-26 20:41:33 +0200

    Add image field in templates

---
+++ features/steps/register_productor.py
@@ -0,0 +1,29 @@
+from behave import *
+import operator
+from django.db.models import Q
+
+use_step_matcher("parse")
+
+
+@when(u'I register movie')
+def step_impl(context):
+    for row in context.table:
+        context.browser.visit(context.get_url('FilmRevolutionApp:create_movie'))
+        form = context.browser.find_by_tag('form').first
+        for heading in row.headings:
+            context.browser.fill(heading, row[heading])
+        form.find_by_value('Submit').first.click()
+
+
+@then(u'I\'m viewing the details page for restaurant')
+def step_impl(context):
+    q_list = [Q((attribute, context.table.rows[0][attribute])) for attribute in context.table.headings]
+    from FilmRevolutionApp.models import Movie
+    movie = Movie.objects.filter(reduce(operator.and_, q_list)).get()
+    assert context.browser.url == context.get_url(movie)
+
+
+@then(u'There are {count:n} restaurants')
+def step_impl(context, count):
+    from FilmRevolutionApp.models import Movie
+    assert count == Movie.objects.count()
